86aae953f90f40fc6f96a5cd54efcefefab91c67
Amrutha0310 <amruthamohandas1410@gmail.com>
2026-02-21 22:55:27 +0530

    initialized Frontend and backend-Navbar created

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
