32f39e6933270b823a794999da541fc35e700216
roshni-spec <roshanipari577@gmail.com>
2026-02-22 02:00:50 +0530

    Login page by FLYONUI and authcontext functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <Toaster/>
       <Routes>
         <Route path="/login" element={<Login />} />
        
