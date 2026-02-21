5e8abcb25485e16a8d432336be01f0936b5e775c
roshni-spec <roshanipari577@gmail.com>
2026-02-22 01:58:13 +0530

    updated Login page and done the functionality, use bcrypt for password hashing, authcontext and config API

---
+++ server/src/models/userModel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "teacher", "admin"],
+      default: "student",
+    },
+    enrolledCourses: [
+      {
+        course: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "course",
+        },
+        progress: {
+          type: Number,
+          default: 0,
+        },
+        completedLessons: [{ type: String }],
+        attendance: { type: Number, default: 0 },
+      },
+    ],
+    learningStreak: {
+      type: Number,
+      default: 0,
+    },
+    lastActivity: {
+      type: Date,
+    },
+    skills: [{ type: String }],
+    performance: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+const User = mongoose.model("USER:", userSchema);
+export default User;
