2abdf5e4e7837aa8824fcf878bb858bc7a8a692c
roshni-spec <roshanipari577@gmail.com>
2026-02-23 16:15:41 +0530

    update database connectivity

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import connectDB from "./src/config/db.js";
 import morgan from "morgan"; //gives error,or any log data in terminal
@@ -39,9 +41,10 @@ app.use((err, req, res, next) => {
 
 // mongoDB connection
 const port =process.env.PORT || 5000;
+ connectDB();
 app.listen(port, async()=>{
     console.log("server port:",port);
-    connectDB();
+   
     try {
         //cloudinary 
     } catch (error) {
